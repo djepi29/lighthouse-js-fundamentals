16505a239c00447ce32690269badc3f891dddbef
djepi29 <j3anpi3rr3g3org3s@email.com>
2022-10-25 10:28:13 -0400

    pending

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+iceCreamFlavours.push("root beer")
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[5]);
+
+console.log(iceCreamFlavours.length);
+
+console.log("bfggtghtg" > "dffggc")
+
+console.log(5 % "3")
